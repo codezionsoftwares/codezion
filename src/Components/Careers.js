bfdb98d037c780113cb63e065fcfe7a6efc0d7aa
codezionsoftwares <codezionsoftwares@gmail.com>
2024-10-03 11:11:41 +0530

    cnhges

--- src/Components/Careers.js
+++ src/Components/Careers.js
@@ -42,110 +42,230 @@ function Careers() {
       <section className='section-padding'>
         <div className="container">
           <div className="row justify-content-center">
-            <div className="col-lg-10">
-              <div className="career-deatils">
-                <div className='text-center mb-5'>
-                <h2 className='fw-600'>Current Openings</h2>
-                <p>Feel fulfilled. Have fun. Help us to shape the future.</p>
+            <div className="col-lg-5">
+              <div className="gallery-images">
+                <img src="assets/images/gallery/1.png" alt="" />
+              </div>
+              <div className="row">
+                <div className="col-lg-5">
+                  <div className="gallery-images">
+                    <img src="assets/images/gallery/2.png" alt=""/>
+                  </div>
                 </div>
-                <div className="career-table table-responsive">
-                  <table className="table table-striped table-hover">
-                    <thead>
-                      <tr>
-                        <th>Role</th>
-                        <th>Location</th>
-                        <th>Minimum Experience</th>
-                        <th>Type</th>
-                        <th></th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      <tr>
-                        <th>HR Generalist</th>
-                        <td>Jaipur, India.</td>
-                        <td>1 years minimum excluding the training period.</td>
-                        <td>Full Time</td>
-                        <td><button className='btn btn-apply'>Apply Now</button></td>
-                     </tr>
-                     <tr>
-                        <th>PHP Developer</th>
-                        <td>Jaipur, India.</td>
-                        <td>1 years minimum excluding the training period.</td>
-                        <td>Full Time</td>
-                        <td><button className='btn btn-apply'>Apply Now</button></td>
-                     </tr>
-                     <tr>
-                        <th>React Native Developer</th>
-                        <td>Jaipur, India.</td>
-                        <td>1.5 years minimum excluding the training period.</td>
-                        <td>Full Time</td>
-                        <td><button className='btn btn-apply'>Apply Now</button></td>
-                     </tr>
-                     <tr>
-                        <th>Laravel Developer</th>
-                        <td>Jaipur, India.</td>
-                        <td>2 years minimum excluding the training period.</td>
-                        <td>Full Time</td>
-                        <td><button className='btn btn-apply'>Apply Now</button></td>
-                     </tr>
-                     <tr>
-                        <th>International Marketing Executive</th>
-                        <td>Jaipur, India.</td>
-                        <td>1 years minimum excluding the training period.</td>
-                        <td>Full Time</td>
-                        <td><button className='btn btn-apply'>Apply Now</button></td>
-                     </tr>
-                    </tbody>
-                  </table>
-                    <p className='text-center'>We are always on the lookout for talented folk to join our team. Follow us on <Link to="#">LinkedIn</Link>!</p>
+                <div className="col-lg-7">
+                  <div className="gallery-images">
+                    <img src="assets/images/gallery/3.png" alt="" />
+                  </div>
                 </div>
+              </div>
+            </div>
+            <div className="col-lg-5">
+              <div className="row">
+              <div className="col-lg-6">
+                <div className="gallery-images">
+                    <img src="assets/images/gallery/4.png" alt="" />
+                  </div>
+                  <div className="gallery-images">
+                    <img src="assets/images/gallery/5.png" alt="" />
+                  </div>
+                </div>
+                <div className="col-lg-6">
+                    <div className="gallery-images">
+                      <img src="assets/images/gallery/6.png" alt="" />
+                    </div>
+              </div>
+              </div>
+            </div>
+          </div>
+          <div className="row mt-5">
+            <div className="col-lg-5">
+              <div className="career-title">
+                <h6>OUR CULTURE</h6>
+                <h4>United by a shared passion for <br /> technology and a drive to <br /> make an impact</h4>
+                <Link to="/contact" className='btn thm-btn text-white rounded-2 px-3 mt-3'>GET IN TOUCH <i className='fa fa-arrow-right'></i></Link>
+              </div>
+            </div>
+            <div className="col-lg-7">
+              <div className="culture border-left">
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquam maxime ratione aspernatur excepturi, maiores magni. Vitae accusamus odit culpa sapiente voluptates, deserunt, possimus incidunt animi expedita ea minus dolor nulla?</p>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquam maxime ratione aspernatur excepturi, maiores magni. Vitae accusamus odit culpa sapiente voluptates, deserunt, possimus incidunt animi expedita ea minus dolor nulla?</p>
               </div>
             </div>
           </div>
           <div className="row pt-5">
-            <div className="col-lg-6">
-              <div className="career-image">
-                <img src="assets/images/career-bg.png" alt="" />
+            <div className="col-lg-4 col-md-6">
+              <div className="culture-box">
+              <i className="fa fa-hand-holding-heart"></i>
+                <h5>Collaboration</h5>
+                <p>We blend a kaleidoscope of talents, sparking a stmphony of creativity that guides us toword breakthroughs.</p>
               </div>
             </div>
-            <div className="col-lg-6">
-              <div className="career-form">
-                <form className="row g-3">
-                  <div className="col-md-6">
-                  <label htmlFor="">Your Name *</label>
-                    <input type="text" className="form-control" id="inputEmail4" placeholder="First Name" />
-                  </div>
-                  <div className="col-md-6">
-                  <label htmlFor="">Email address *</label>
-                    <input type="text" className="form-control" id="inputEmail4" placeholder="Last Name" />
-                  </div>
-                  <div className="col-md-6">
-                  <label htmlFor="">Your Name *</label>
-                    <input type="email" className="form-control" id="inputPassword4" placeholder="Email" />
-                  </div>
-                  <div className="col-md-6">
-                  <label htmlFor="">Phone number</label>
-                    <input type="text" className="form-control" id="inputEmail4" placeholder="Mobile No." />
-                  </div>
-                  <div className="col-md-6">
-                  <label htmlFor="">Post *</label>
-                    <input type="text" className="form-control" id="inputEmail4" placeholder="Position" />
-                  </div>
-                  <div className="col-md-6">
-                  <label htmlFor="">Resume *</label>
-                    <input type="file" className="form-control" id="inputEmail4" placeholder="No File chosen" />
-                  </div>
-                  <div className="col-12">
-                  <label htmlFor="">Message</label>
-                    <textarea name="" id="" rows="5" className="outline-0 border-light-subtle rounded-3" placeholder='Your Message'></textarea>
-                  </div>
+            <div className="col-lg-4 col-md-6">
+              <div className="culture-box">
+              <i className="fa fa-hand-holding-heart"></i>
+                <h5>Innovation</h5>
+                <p>We blend a kaleidoscope of talents, sparking a stmphony of creativity that guides us toword breakthroughs.</p>
+              </div>
+            </div>
+            <div className="col-lg-4 col-md-6">
+              <div className="culture-box">
+              <i className="fa fa-hand-holding-heart"></i>
+                <h5>Empowerment</h5>
+                <p>We blend a kaleidoscope of talents, sparking a stmphony of creativity that guides us toword breakthroughs.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className='section-padding p-0'>
+        <div className="row">
+          <div className="col-lg-3 col-md-6 p-0 d-flex align-items-center ">
+              <div className="p-5 recruitment-box">
+                <div>
+                <h3>Recruitment Process</h3>
+                <p>We do the hiring process very smoothly. which is completely dependent on the caliber of the condidtae.</p>
+                </div>
+              </div>
+          </div>
+          <div className="col-lg-3 col-md-6 p-0">
+            <div className="recruitment-image">
+              <img src="assets/images/gallery/cv.png" alt="" />
+              <div className="recuir-title">
+                <h2>01</h2>
+                <h6>CV Screening</h6>
+              </div>
+            </div>
+          </div>
+          <div className="col-lg-3 col-md-6 p-0">
+            <div className="recruitment-image">
+              <img src="assets/images/gallery/interview.png" alt="" />
+              <div className="recuir-title">
+                <h2>02</h2>
+                <h6>Technical Interview</h6>
+              </div>
+            </div>
+          </div>
+          <div className="col-lg-3 col-md-6 p-0">
+            <div className="recruitment-image">
+              <img src="assets/images/gallery/hr.png" alt="" />
+              <div className="recuir-title">
+                <h2>03</h2>
+                <h6>HR Round</h6>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className='section-padding pb-0'>
+        <div className="container">
+        <div className="join-title text-center">
+            <h3 className='py-4'>Why Join HawksCode?</h3>
+          </div>
+          <div className="row">
+            <div className="col-lg-3 col-md-4 col-sm-6">
+              <div className="join-card">
+              <img src="assets/images/icons/join1.png" alt="" />
+                <div className="howkacode-text">
+                  <h5>Cutting-Edge-Projects</h5>
+                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus dicta id hic doloremque! Facere in explicabo </p>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-3 col-md-4 col-sm-6">
+              <div className="join-card">
+              <img src="assets/images/icons/join2.png" alt="" />
+                <div className="howkacode-text">
+                  <h5>Career Growth</h5>
+                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus dicta id hic doloremque! Facere in explicabo </p>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-3 col-md-4 col-sm-6">
+              <div className="join-card">
+              <img src="assets/images/icons/join3.png" alt="" />
+                <div className="howkacode-text">
+                  <h5>Competitive Compensation</h5>
+                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus dicta id hic doloremque! Facere in explicabo </p>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-3 col-md-4 col-sm-6">
+              <div className="join-card">
+              <img src="assets/images/icons/join4.png" alt="" />
+                <div className="howkacode-text">
+                  <h5>Comprehensive Benefits</h5>
+                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus dicta id hic doloremque! Facere in explicabo </p>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-3 col-md-4 col-sm-6">
+              <div className="join-card">
+              <img src="assets/images/icons/join5.png" alt="" />
+                <div className="howkacode-text">
+                  <h5>Global Exposure</h5>
+                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus dicta id hic doloremque! Facere in explicabo </p>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-3 col-md-4 col-sm-6">
+              <div className="join-card">
+              <img src="assets/images/icons/join6.png" alt="" />
+                <div className="howkacode-text">
+                  <h5>Learning Opportunities</h5>
+                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus dicta id hic doloremque! Facere in explicabo </p>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-3 col-md-4 col-sm-6">
+              <div className="join-card">
+              <img src="assets/images/icons/join7.png" alt="" />
+                <div className="howkacode-text">
+                  <h5>Startup Culture with Stability</h5>
+                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus dicta id hic doloremque! Facere in explicabo </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
 
-                  <div className="col-12 text-center">
-                    <button type="submit" className="btn thm-btn text-white rounded-5 btn-lg w-100 px-5">
-                      Submit Your Request
-                    </button>
-                  </div>
-                </form>
+      <section className='section-padding'>
+        <div className="container">
+        <div className="section-headding text-center mb-5">
+          <h2>What's your Forte?</h2>
+        </div>
+          <div className="row">
+            <div className="col-lg-3 col-sm-6">
+              <div className="fort-box">
+              <img src="assets/images/icons/briefcase.png" alt="" />
+                <h6>Business Analyst</h6>
+                <p>Role & Responsibility</p>
+                <Link to="/careerdetails" className='btn-thm-main sm-btn'>Apply</Link>
+              </div>
+            </div>
+            <div className="col-lg-3 col-sm-6">
+              <div className="fort-box border-left">
+              <img src="assets/images/icons/digital-marketing.png" alt="" />
+                <h6>Digital Marketing</h6>
+                <p>Role & Responsibility</p>
+                <Link to="/careerdetails" className='btn-thm-main sm-btn'>Apply</Link>
+              </div>
+            </div>
+            <div className="col-lg-3 col-sm-6">
+              <div className="fort-box border-left">
+              <img src="assets/images/icons/python.png" alt="" />
+                <h6>Python Developer</h6>
+                <p>Role & Responsibility</p>
+                <Link to="/careerdetails" className='btn-thm-main sm-btn'>Apply</Link>
+              </div>
+            </div>
+            <div className="col-lg-3 col-sm-6">
+              <div className="fort-box border-left">
+              {/* <i className="fab fa-laravel"></i> */}
+              <img src="assets/images/icons/web-development.png" alt="" />
+                <h6>Larawel Developer</h6>
+                <p>Role & Responsibility</p>
+                <Link to="/careerdetails" className='btn-thm-main sm-btn'>Apply</Link>
               </div>
             </div>
           </div>
