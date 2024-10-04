b076daba617ae32f6d2d128ff0f2ebd293c7c647
codezionsoftwares <codezionsoftwares@gmail.com>
2024-10-04 10:41:10 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ import CareerDetails from './Components/CareerDetails';
 
 function App() {
   return (
-    <BrowserRouter basename='codezion'>
+    <BrowserRouter>
       <Routes>
         <Route path='/' element={<Home/>} />
         <Route path='/about' element={<About/>} />
